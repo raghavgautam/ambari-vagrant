32d59e39624e46bd751317c380f2e7106758ac4f
Raghav Kumar Gautam <raghav@apache.org>
2016-06-17 13:34:21 -0700

    adding vagrant status command.

--- centos6.4/automate.sh
+++ centos6.4/automate.sh
@@ -3,6 +3,7 @@ NUM_MACHINES=3
 MACHINES=$(eval echo c640{1..${NUM_MACHINES}}.ambari.apache.org)
 echo bringing up $MACHINES
 ./up.sh ${NUM_MACHINES}
+vagrant status
 #exec vagrant ssh c6401
 osascript -e 'display notification "Open commandline for next steps." with title "Ambari vagrant is ready"' &
 
@@ -10,6 +11,6 @@ echo Going to copy following machine details for ambari to clipboard && echo ${M
 echo "Press enter to continue" && read
 echo  "${MACHINES}" | tr ' ' '\n' | pbcopy
 
-echo opening ambari ui && sleep 5 && open "http://c6401.ambari.apache.org:8080/"
+echo opening ambari ui && sleep 2 && open "http://c6401.ambari.apache.org:8080/"
 echo "Going to copy ssh key to clipboard" && read
 pbcopy <insecure_private_key
